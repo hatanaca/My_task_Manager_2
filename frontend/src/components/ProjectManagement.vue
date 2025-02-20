92eb70396b608f737ad4eb222f4d2832184e66ef
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-20 12:54:34 -0300

    editando ProjectManagement

---
+++ frontend/src/components/ProjectManagement.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>Project Management</h2>
+		<form @submit.prevent="createProject">
+			<input v-model="newProject.name" placeholder="Project Name" required />
+			<textarea v-model="newProject.description" placeholder="Project Description"></textarea>
+			<button type="submit">Create Project</button>
+		</form>
+		<ul>
+			<li v-for"project in project" :key="project">
+				<strong>{{ project.name }}</strong>: {{ project.description }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+
