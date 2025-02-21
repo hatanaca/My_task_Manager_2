297294000baedca43313b5767464eaab4393f1eb
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-21 14:20:27 -0300

    finalizado ProjectManagement

--- frontend/src/components/ProjectManagement.vue
+++ frontend/src/components/ProjectManagement.vue
@@ -7,11 +7,48 @@
 			<button type="submit">Create Project</button>
 		</form>
 		<ul>
-			<li v-for"project in project" :key="project">
+			<li v-for="project in project" :key="project">
 				<strong>{{ project.name }}</strong>: {{ project.description }}
 			</li>
 		</ul>
 	</div>
 </template>
 
+<script lang="ts">
+	import {defineComponent, ref, onMounted } from 'vue';
+	import axios from 'axios';
 
+	interface Project {
+		id: number;
+		name: string;
+		description: string;
+		}
+
+	export default defineComponent({
+		name: 'ProjectManagement',
+		setup() { 
+			const projects = ref<Project[]>([]);
+			const newProject = ref({name: '', description: '' });
+			const fetchProjects = async () => {
+				try {
+				const response = await axios.get('http://localhost/api/projects');
+				projects.value = response.data;
+				} catch (error) {
+				console.log('Error fetching projects', error)
+				}
+			};
+			const createProject = async () => {
+				try {
+				await axios.post('http://localhost/api/projects', newProject.value);
+				newProject.value = {name: '', description: '' };
+				fetchProjects();
+				} catch (error) {
+				console.error('Error creating project', error);
+				}
+			};
+
+			onMounted(fetchProjects);
+			return { projects, newProject, createProject};
+			}
+		});
+</script>
