af5ebd45445d5792150659b553163be5367446b0
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-03-04 13:36:22 -0300

    aprendendo sobre route

---
+++ backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AttachmentController;
+use App\Http\Controllers\ProjectController;
+
+
