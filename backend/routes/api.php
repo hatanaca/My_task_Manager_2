0d582aafbd06a38c10f2246cd186a487b8e3ca31
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-03-05 14:22:08 -0300

    finalizando routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,4 +8,30 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\AttachmentController;
 use App\Http\Controllers\ProjectController;
 
+//tasks routes
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks/{id}', [TaskController::class, 'show']);
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TasController::class, 'destroy']);
 
+
+//Comments routes for task
+Route::get('/tasks/{taskId}/comments', [CommentController::class, 'index']);
+Route::post('/tasks/{taskId}/comments', [CommentController::class, 'store']);
+
+
+//File attachments for task
+Route::post('/tasks/{taskId}/attachments', [AttachmentController::class, 'store']);
+
+//Project routes
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::post('/projects', [ProjectController::class, 'store']);
+
+
+//User management routes protected by acess control middleware
+Route::middleware('access.control:admin')->group(function () {
+	Route::get('/users', [UserController::class, 'index']);
+	Route::put('/users/{id}', [UserController::class, 'update']);
+	Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
